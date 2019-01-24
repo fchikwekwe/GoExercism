c01f5c15818cc0dc3d1b42d8a4e440b15bc83cd2
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-01-24 14:37:31 -0800

    📝 added README.md

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,19 @@
 package hamming
 
+import "fmt"
+
 func Distance(a, b string) (int, error) {
+    hammingDistance := 0
+    if len(a) == len(b) {
+        for index := range a {
+            aIndex := a[index]
+            for index := range b {
+                if aIndex == b[index] {
+                    fmt.Println("a value: %v, b value: %v", aIndex, b[index])
+                    hammingDistance += 1
+                }
+            }
+        }
+    }
+    return hammingDistance, fmt.Errorf("cannot compare; strings are not the same length")
 }
