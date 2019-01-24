49633df2dfcb126cf9c7dd1e91857e82bf4e4844
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-01-24 14:23:19 -0800

    init

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,11 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package hello_world returns a string hello world
+// https://golang.org/doc/effective_go.html#commentary
+package greeting
+
+// HelloWorld returns a string that says "Hello, World!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,13 @@
+// Package twofer returns a string that either replaces X with a name or
+// replaces X with you.
+package twofer
+
+import "fmt"
+// ShareWith checks to see if we should return a string with name or with you
+func ShareWith(name string) string {
+	if name != "" {
+		return fmt.Sprintf("One for %v, one for me.", name)
+	} else {
+		return fmt.Sprintf("One for you, one for me.")
+	}
+}
