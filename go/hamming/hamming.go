babf571b6d932f42444118b8f2f5dc467f8dc42d
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-01-29 13:51:23 -0800

    ✏  added clarifying print statements to hamming exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -8,14 +8,14 @@ func Distance(a, b string) (int, error) {
     hammingDistance := 0
     error := fmt.Errorf("cannot compare; strings are not the same length")
     if len(a) == len(b) {
-        fmt.Printf("a length: %v, b length: %v", len(a), len(b))
+        // fmt.Printf("a length: %v, b length: %v", len(a), len(b))
         if len(a) == 0 {
             return hammingDistance, nil
         } else {
             for aIndex, aValue := range a {
                 for bIndex, bValue := range b {
                     if aIndex == bIndex && aValue != bValue {
-                        fmt.Printf("\na value: %v, b value: %v", aValue, bValue)
+                        // fmt.Printf("\na value: %v, b value: %v", aValue, bValue)
                         hammingDistance += 1
                     }
                 }
@@ -24,5 +24,6 @@ func Distance(a, b string) (int, error) {
     } else {
         return hammingDistance, error
     }
+    // fmt.Printf("hamming: %v ", hammingDistance)
     return hammingDistance, nil
 }
