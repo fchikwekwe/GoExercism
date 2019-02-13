a06726602539e810997f1a6dbda42d5d90835dc8
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-02-12 17:17:39 -0800

    ✒️ more fixes

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -1,7 +1,7 @@
 // This is a "stub" file.  It's a little start on your solution.
 // It's not a complete solution though; you have to write some code.
 
-// Package hello_world returns a string hello world
+// Package greeting returns a string hello world
 // https://golang.org/doc/effective_go.html#commentary
 package greeting
 
--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,9 +1,9 @@
-package isogram
-
-// IsIsogram checks to see if the input word has any repeating letters exclusing hyphens and spaces
-func IsIsogram (word string) (bool) {
-    var m map[string]
-    for i := range word {
-
-    }
-}
+// package isogram
+//
+// // IsIsogram checks to see if the input word has any repeating letters exclusing hyphens and spaces
+// func IsIsogram (word string) (bool) {
+//     var m map[string]
+//     for i := range word {
+//
+//     }
+// }
