a0570515482c7a93a31f512a0cfce8c9ae83cdfd
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-02-14 14:32:46 -0800

    🏃🏾‍♀️ still working on luhn algorithm implementation

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -12,10 +13,35 @@ func Valid(number string) bool {
 		return false
 	}
 	number = strings.Replace(number, " ", "", -1)
-	fmt.Println(number)
-	return true
+	numArr := strings.Split(number, "")
+
+	var sum int
+
+	for i := len(number); i > -1; i-- {
+		num, err := strconv.Atoi(numArr[i])
+		// Check to see if all values in string are valid digits
+		if err != nil {
+			return false
+		}
+		if i%2 == 0 {
+			if num <= 4 {
+				sum += num * 2
+				fmt.Println("odd, small", numArr[i], sum)
+			} else if num > 4 {
+				sum += num*2 - 9
+				fmt.Println("odd, large", numArr[i], sum)
+			}
+		} else {
+			sum += num
+			fmt.Println("even, _", numArr[i], sum)
+		}
+	}
+	if sum%10 == 0 {
+		return true
+	}
+	return false
 }
 
 func main() {
-	Valid("0 91")
+	fmt.Println(Valid("0 91"))
 }
