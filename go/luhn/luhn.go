08fae5ba286df5e001abeb218eef019525bc9c9f
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-02-19 18:31:03 -0800

    🔢 luhn completed

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -8,41 +8,44 @@ import (
 
 // Valid is a function that takes in an int and returns a bool. This bool describes whether the number is valid per the Luhn formula.
 func Valid(number string) bool {
+	number = strings.Replace(number, " ", "", -1)
+
 	// Check to see if the string length is <= 1
-	if len(number) <= 1 {
+	if len(number) < 2 {
 		return false
 	}
-	number = strings.Replace(number, " ", "", -1)
-	numArr := strings.Split(number, "")
 
-	var sum int
+	var sum int64
+	isOdd := len(number)%2 != 0
 
-	for i := len(number); i > -1; i-- {
-		fmt.Println(i)
-		num, err := strconv.Atoi(numArr[i])
+	for i := len(number) - 1; i >= 0; i-- {
+		num, err := strconv.ParseInt(number[i:i+1], 0, 64)
 		// Check to see if all values in string are valid digits
 		if err != nil {
 			return false
 		}
-		if i%2 == 0 {
-			if num <= 4 {
-				sum += num * 2
-				fmt.Println("odd, small", numArr[i], sum)
-			} else if num > 4 {
-				sum += num*2 - 9
-				fmt.Println("odd, large", numArr[i], sum)
+
+		if isOdd {
+			if i%2 != 0 {
+				num *= 2
+				if num > 9 {
+					num -= 9
+				}
 			}
+
 		} else {
-			sum += num
-			fmt.Println("even, _", numArr[i], sum)
+			if i%2 == 0 {
+				num *= 2
+				if num > 9 {
+					num -= 9
+				}
+			}
 		}
+		sum += num
 	}
-	if sum%10 == 0 {
-		return true
-	}
-	return false
+	return (sum%10 == 0)
 }
 
 func main() {
-	fmt.Println(Valid("0 91"))
+	fmt.Println(Valid("059"))
 }
