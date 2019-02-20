de792037666dcc52750263993bd22715db7fa38e
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-02-19 18:36:25 -0800

    👍🏾 minor update to luhn function

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,34 +1,35 @@
 package isogram
 
 import (
-    "strings"
-    "fmt"
+	"fmt"
+	"strings"
 )
+
 // IsIsogram is a function that takes in a word as a string and outputs a bool
 // letting you know whether the word is an isogram.
-func IsIsogram (word string) (bool) {
-    // Make all letters in word the same case
-    uWord := strings.ToLower(word)
-    fmt.Println("Word", uWord)
+func IsIsogram(word string) bool {
+	// Make all letters in word the same case
+	uWord := strings.ToLower(word)
+	fmt.Println("Word", uWord)
 
-    // Initialize map variable
-    m := make(map[byte]int)
+	// Initialize map variable
+	m := make(map[byte]int)
 
-    // Iterate over letters in the word
-    for i := range word {
-        if _, ok := m[uWord[i]]; ok {
-            // Exclude hyphens and spaces from map
-            if uWord[i] != 45 && uWord[i] != 32 {
-                m[uWord[i]] += 1
-            }
-        } else {
-            m[uWord[i]] = 1
-        }
-    }
-    for _, v := range m {
-        if v != 1 {
-            return false
-        }
-    }
-    return true
+	// Iterate over letters in the word
+	for i := range word {
+		if _, ok := m[uWord[i]]; ok {
+			// Exclude hyphens and spaces from map
+			if uWord[i] != 45 && uWord[i] != 32 {
+				m[uWord[i]]++
+			}
+		} else {
+			m[uWord[i]] = 1
+		}
+	}
+	for _, v := range m {
+		if v != 1 {
+			return false
+		}
+	}
+	return true
 }
--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,30 +1,30 @@
 package raindrops
 
 import (
-    "strconv"
-    "fmt"
-    )
+	"fmt"
+	"strconv"
+)
 
 // Convert is a FizzBuzz-like function that checks to see if the value is divisible
 // by 3 (return 'Pling'), by 5 (returns 'Plang') or by 7 (returns 'Plong')
-func Convert (drop int) (string) {
-    results := ""
-    if drop % 3 == 0 {
-        results += "Pling"
-    }
-    if drop % 5 == 0 {
-        results += "Plang"
-    }
-    if drop % 7 == 0 {
-        results += "Plong"
-    }
+func Convert(drop int) string {
+	results := ""
+	if drop%3 == 0 {
+		results += "Pling"
+	}
+	if drop%5 == 0 {
+		results += "Plang"
+	}
+	if drop%7 == 0 {
+		results += "Plong"
+	}
 
-    if len(results) == 0 {
-        results += strconv.Itoa(drop)
-    }
-    return results
+	if len(results) == 0 {
+		results += strconv.Itoa(drop)
+	}
+	return results
 }
 
 func main() {
-    fmt.Println(Convert(3))
+	fmt.Println(Convert(3))
 }
